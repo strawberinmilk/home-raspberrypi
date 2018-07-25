485095d0631159fbb7487340fb88a1f074a428f7
rinkeisgithub <rinkei.krt6006@gmail.com>
2018-07-25 13:19:08 +0900

    settimeout

--- index.js
+++ index.js
@@ -2,27 +2,48 @@
 
 const fs = require('fs');
 const pash = '/sys/class/gpio/';
-const usePin = [25];
-if(process.platform!='darwin'){
-  for(let i=0;i<usePin.length;i++){
-    try{
-      fs.writeFileSync(`${pash}export`, 25);
-    }catch(e){
+const usePin = [];
+let timeout = [];
+
+const setPin = (pin)=>{
+  if(process.platform!='darwin'){
+    for(let i=0;i<usePin.length;i++){
+      try{
+        fs.writeFileSync(`${pash}export`, pin);
+      }catch(e){
+      }  
+      fs.writeFileSync(`${pash}gpio${pin}/direction`, 'out');
     }  
-    fs.writeFileSync(`${pash}gpio25/direction`, 'out');
   }  
 }
 
 const lightSwitch = (pin,num) =>{
+  if(usePin.indexOf(pin,0)===-1) setPin(pin)
+  clearTimeout(timeout[pin])  
   if(process.platform!='darwin') fs.writeFileSync(`${pash}gpio${pin}/value`, num);
   console.log(`${pin} ${num}`)
 }
 
 const http = require('http');
 http.createServer((req, res) => {
-  console.log(req.url.replace(/\/\?/,"").replace(/%22/g,'"')+'')
-  let r = JSON.parse(req.url.replace(/\/\?/,"").replace(/%22/g,'"')+'')
-  lightSwitch(r.pin,r.num);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  let r
+  try{
+    r = JSON.parse(req.url.replace(/\/\?/,"").replace(/%22/g,'"')+'')
+  }catch(e){
+  }
+  if(r&&r.pin+1&&r.num+1){
+    let text = `pin:${r.pin} num:${r.num} `
+    if(r.time){
+      clearTimeout(timeout[r.pin]);
+      timeout[r.pin] = setTimeout(()=>{lightSwitch(r.pin,r.num)},r.time);
+      text += ` timeout:${r.time}`;
+    }else{
+      lightSwitch(r.pin,r.num);
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(text);
+  }else{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`The request was successful.\nBut the query is broken.`);
+  }
 }).listen(9001);
