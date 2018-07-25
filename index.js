9520b5934fdbed6be826c5c4605b853aef97c34a
rinkeisgithub <rinkei.krt6006@gmail.com>
2018-07-25 21:14:23 +0900

    0725night

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const setPin = (pin)=>{
 }
 
 const lightSwitch = (pin,num) =>{
-  if(usePin.indexOf(pin,0)===-1) setPin(pin)
-  clearTimeout(timeout[pin])  
+  if(usePin.indexOf(pin,0)===-1) setPin(pin);
+  clearTimeout(timeout[pin]);
   if(process.platform!='darwin') fs.writeFileSync(`${pash}gpio${pin}/value`, num);
-  console.log(`${pin} ${num}`)
+  console.log(`${pin} ${num}`);
 }
 
 const http = require('http');
 http.createServer((req, res) => {
-  let r
+  let r;
   try{
-    r = JSON.parse(req.url.replace(/\/\?/,"").replace(/%22/g,'"')+'')
+    r = JSON.parse(req.url.replace(/\/\?/,"").replace(/%22/g,'"')+'');
   }catch(e){
   }
   if(r&&r.pin+1&&r.num+1){
-    let text = `pin:${r.pin} num:${r.num} `
+    let text = `pin:${r.pin} num:${r.num} `;
     if(r.time){
       clearTimeout(timeout[r.pin]);
       timeout[r.pin] = setTimeout(()=>{lightSwitch(r.pin,r.num)},r.time);
---
+++ test.js
@@ -0,0 +1,14 @@
+let rgb = [0,300,900]
+let i=0
+setInterval(()=>{
+  console.log("にゃん")
+  if(i=1000)i=0
+  for(let j=0;j<3;j++){
+    if(rgb[j]<i){
+
+    }else{
+
+    }
+  }
+  i++
+},0)
