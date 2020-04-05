b4b9243d9ab2b408682ac61bb3efb9b4d1c7ebd1
rinkei <rinkei.krt6006@gmail.com>
2020-04-05 21:32:51 +0900

    changeServoAngle

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const lightSwitch = (num) =>{
     console.log(`${path}gpio25/value,${num}`)
     fs.writeFileSync(`${path}gpio25/value`, num)
     servo.open().then(()=>{
-      servo.setDegree(num?120:60)
+      servo.setDegree(num?130:50)
     })
     setTimeout(()=>{
       servo.open().then(()=>{
